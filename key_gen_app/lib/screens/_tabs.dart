415d7a891e17f3904c62b27b01d9366c923d709c
SerpenTaki <gabrielecorubolo@gmail.com>
2025-09-21 14:40:52 +0200

    Update _tabs.dart

--- key_gen_app/lib/screens/_tabs.dart
+++ key_gen_app/lib/screens/_tabs.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:key_gen_app/screens/PrimaPagina.dart';
+import 'package:key_gen_app/screens/Carta.dart';
+import 'package:key_gen_app/screens/Sliddare.dart';
 
 class Tabbare extends StatefulWidget {
   const Tabbare({super.key});
@@ -22,19 +25,40 @@ class _TabbareState extends State<Tabbare> {
             fontSize: 20,
           ),
           centerTitle: true,
-          bottom: const TabBar(
+          flexibleSpace: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+             // Text("Flutter Tabs Demo", textAlign: TextAlign.center,),
+              //Se metti qui la tabbar non hai problemi e non fa lo spazio sopra brutto
+            ],
+          ),
+          bottom: TabBar(
+            //isScrollable: true, //per tanti elementi tipo 7 e otto
              // indicatorColor: Colors.white,
               labelColor: Colors.white,
               unselectedLabelColor: Colors.grey,
               automaticIndicatorColorAdjustment: true,
               dividerColor: Colors.orange,
-              tabs: [
+              //indicator: BoxDecoration(
+              //  borderRadius: BorderRadius.circular(50),
+              //  color: Colors.blue,
+              //),
+              tabs: const [
                 Tab(icon: Icon(Icons.directions_bike, color: Colors.white,), text: "bike",),
                 Tab(icon: Icon(Icons.directions_bus, color: Colors.white,), text: "bus",),
                 Tab(icon: Icon(Icons.directions_boat, color: Colors.white,), text: "Boat",),
               ]),
         ),
-        body: null,
+        body: TabBarView(
+          children: [
+            //Primapagina(),
+            //Carta(),
+            //Sliddare(), //Attenzione che importa l'intera pagina con tutti i suoi widget, floating action button e appbar
+            Icon(Icons.directions_bike, color: Colors.black,),
+            Icon(Icons.directions_bus, color: Colors.black,),
+            Icon(Icons.directions_boat, color: Colors.black,),
+          ],
+        ),
       ),
     );
   }
